a9c5cce13c7359cf7451a867a53194bbee54e1c7
ReneNulschDE <ReneNulschDE@Nulsch.de>
2017-12-22 19:04:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+    name='mercedesmejsonpy',
+    version='0.0.1',
+    packages=['mercedesjsonpy'],
+    include_package_data=True,
+    python_requires='>=3',
+    license='WTFPL',
+    description='A library to work with Mercedes ME API.',
+    long_description='A library to work with Mercedes ME car API.',
+    url='https://github.com/ReneNulschDE/mercedesmejsonpy',
+    author='Rene Nulsch',
+    author_email='github.mercedesmejsonpy@nulsch.de',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+    ],
+)
